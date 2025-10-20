8248f1dc72e853dfa1f7d902224dbc719b00b51b
Julien <julien@testaz.ch>
2025-10-20 19:23:36 +0200

    feat(core): initialize Spring Boot project with Maven configuration

---
+++ src/main/java/ch/testaz/technicalexercisevaudoise/TechnicalExerciseVaudoiseApplication.java
@@ -0,0 +1,13 @@
+package ch.testaz.technicalexercisevaudoise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalExerciseVaudoiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalExerciseVaudoiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/testaz/technicalexercisevaudoise/TechnicalExerciseVaudoiseApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.testaz.technicalexercisevaudoise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalExerciseVaudoiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
